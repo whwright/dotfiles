c32b36e1ab9affb48e965ce894e828d04fe65174
W. Harrison Wright <harrison@blacklocus.com>
2016-04-11 14:15:54 -0500

    cleanup old build variables, add GOPATH, add omf

--- Darwin/fish/config.symlink/fish/config.fish
+++ Darwin/fish/config.symlink/fish/config.fish
@@ -1,20 +1,16 @@
 . ~/.config/fish/aliases.fish
 
-# TODO: do I still need this?
-ulimit -n 65536
-ulimit -u 2048
-
 set -x EDITOR vim
 set -x PATH /usr/local/bin $PATH
 
-# BUILD VARIABLES
-# TODO: cleanup
-set -x JAVA_HOME_7 /Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home
-set -x JAVA_HOME_6 /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-set -x JAVA_HOME $JAVA_HOME_7
-set -x ECLIPSE_HOME ~/Development/eclipse-clean
-set -x ANDROID_HOME /Users/wrighthw/Library/Android/sdk
-set -x MAVEN_OPTS "-Xmx512m -XX:MaxPermSize=512m"
+set -x GOPATH ~/go
+
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/harrison/.local/share/omf"
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/harrison/.config/omf"
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
 # GIT PROMPT CONFIG
 set -g __fish_git_prompt_show_informative_status 1
