4465dd14fb57c591888e10c08a9dfa9cb3a0406e
W. Harrison Wright <harrison@blacklocus.com>
2016-05-31 12:33:57 -0500

    fixed virtualenv wrapper

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,20 +1,19 @@
 function fish_prompt
-    fish_prompt_short
-end
-
-function fish_prompt_short
     set -l last_status $status
 
-    set_color normal
-    printf "%s" (whoami)
-    printf "@"
-    printf "%s " (hostname|cut -d . -f 1)
+    if test $VIRTUAL_ENV
+        type '_old_fish_prompt' > /dev/null 2>&1
+        if test $status -ne 0
+            set_color normal
+            printf '('
+            set_color -o white
+            printf '%s' (basename "$VIRTUAL_ENV")
+            set_color normal
+            printf ') '
+        end
+    end
 
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-
-    set_color normal
-    printf ' %s ' (__fish_git_prompt)
+    fish_prompt_short
 
     set color normal
     if not test $last_status -eq 0
@@ -27,13 +26,20 @@ function fish_prompt_short
     set_color normal
 end
 
+function fish_prompt_short
+    set_color normal
+    printf "%s" (whoami)
+    printf "@"
+    printf "%s " (hostname|cut -d . -f 1)
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+
+    set_color normal
+    printf ' %s ' (__fish_git_prompt)
+end
+
 function fish_prompt_long
-    set -l last_status $status
-
-    if set -q VIRTUAL_ENV
-        echo -n -s "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-    end
-
     set_color yellow
     printf '[%s] ' (date +%X)
     printf '%s' (whoami)
@@ -52,14 +58,4 @@ function fish_prompt_long
 
     set_color normal
     printf ' %s ' (__fish_git_prompt)
-
-    set color normal
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
-
-    # Line 2
-    echo
-    printf '↪ '
-    set_color normal
 end
