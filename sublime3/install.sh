1606378a624b6c12f1903d506b35b6abf4af9042
W. Harrison Wright <harrison@blacklocus.com>
2016-05-31 16:08:43 -0500

    fixed sublime3 install

--- sublime3/install.sh
+++ sublime3/install.sh
@@ -6,16 +6,16 @@
 info "running sublime3/install.sh"
 
 if [ `uname -s` == 'Darwin' ]; then
-    sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+    sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 3
 elif [ `uname -s` == "Linux" ]; then
-    sublime_dir=~/.config/"sublime-text-3"
+    sublime_dir=~/.config/sublime-text-3
 else
     fail "unsupported operating system"
 fi
 
 if [ ! -L "$sublime_dir/Packages/User" ]; then
     mv "$sublime_dir/Packages/User" "$sublime_dir/Packages/User.backup"
-    ln -s ~/.dotfiles/sublime3/User "$sublime_dir/Packages"
+    ln -s ~/.dotfiles/sublime3/User "$sublime_dir/Packages/User"
     success "sublime3 installed"
 else
     info "sublime3 already installed"
