111b0dda8b7b381fd49420e4b9376ef679cf915f
W. Harrison Wright <harrison@blacklocus.com>
2016-06-01 17:05:25 -0500

    added error message to python install.sh; closes #4

--- python/install.sh
+++ python/install.sh
@@ -11,6 +11,10 @@ for MODULE in $MODULES; do
 
     sudo -H pip install --upgrade "$MODULE"
 
+    if [ $? -ne 0 ]; then
+        fail "error occurred while installed $MODULE"
+    fi
+
     # TODO: revisit if we want to get rid of the password prompt
     #if ! pip freeze | grep $MODULE > /dev/null; then
     #    sudo -H pip install --upgrade $MODULE
