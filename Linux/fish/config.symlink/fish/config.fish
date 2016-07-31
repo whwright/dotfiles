5f2798c2d26c8e3d53a8b781eeeff4a65081a0e3
Harrison Wright <wright8191@gmail.com>
2016-07-31 01:27:26 -0500

    added pyenv to linux config

--- Linux/fish/config.symlink/fish/config.fish
+++ Linux/fish/config.symlink/fish/config.fish
@@ -9,3 +9,9 @@ set -x JAVA_HOME /usr/lib/jvm/java-7-openjdk-amd64
 
 alias tcli "truecrypt-cli"
 alias dlc "deluge-console"
+
+# pyenv
+set -x PATH "/home/wright8191/.pyenv/bin" $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
