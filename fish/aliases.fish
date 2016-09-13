e3853dcec6c476a992f6558aadac6d1bbf0cd60a
Harrison Wright <harrison@blacklocus.com>
2016-09-13 16:06:19 -0500

    git tag timestamp alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -12,6 +12,7 @@ alias gb "git branch"
 alias gl "git log"
 alias gll "git log --graph --oneline --all --decorate -n 25"
 alias glll "git log --graph --oneline --all --decorate"
+alias gtagtime "git log --tags --simplify-by-decoration --pretty='format:%ai %d'"
 
 
 # python
