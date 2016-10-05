63d71711518023f009f2644ade2fc21324c1d709
Harrison Wright <harrison@blacklocus.com>
2016-10-05 13:59:30 -0500

    curl shouldnt fail

--- javarepl/install.sh
+++ javarepl/install.sh
@@ -3,4 +3,4 @@
 # is this a bad idea?
 DOWNLOAD_LINK="http://albertlatacz.published.s3.amazonaws.com/javarepl/javarepl.jar"
 
-curl --fail --silent --show-error ${DOWNLOAD_LINK} --output /opt/javarepl.jar
+curl --silent --show-error ${DOWNLOAD_LINK} --output /opt/javarepl.jar
