d6ede11c2d2b70079bb5bd7dd5726d8a44a5cfaf
W. Harrison Wright <wright8191@gmail.com>
2016-11-04 23:41:45 -0500

    Add terminator to apt-get installs

--- Linux/install.sh
+++ Linux/install.sh
@@ -5,7 +5,7 @@
 DOTFILES_ROOT="$(pwd)"
 
 # install apt-get dependencies
-APT_GET_DEPENDS=("python-dev" "python-pip" "python3-dev" "python3-pip" "vim" "git" "curl")
+APT_GET_DEPENDS=("python-dev" "python-pip" "python3-dev" "python3-pip" "vim" "git" "curl" "terminator")
 sudo apt-get update
 for ITEM in "${APT_GET_DEPENDS[@]}"; do
 	echo "Installing ${ITEM}"
