32beb2ea63b57f1f068d19293aaed17d3f720f89
W. Harrison Wright <wright8191@gmail.com>
2016-11-04 22:29:55 -0500

    Update git aliases to abbrs

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -6,15 +6,6 @@ alias pubip "curl http://canihazip.com/s/; echo ''"
 alias cll "clear; ll"
 
 
-# git
-alias g "git status -sb"
-alias gb "git branch"
-alias gl "git log"
-alias gll "git log --graph --oneline --all --decorate -n 25"
-alias glll "git log --graph --oneline --all --decorate"
-alias gtagtime "git log --tags --simplify-by-decoration --pretty='format:%ai %d'"
-
-
 # python
 alias veinit "virtualenv -p python3 venv; veon"
 alias veon ". venv/bin/activate.fish"
