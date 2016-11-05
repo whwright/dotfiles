afb73ec9c9aa21f8b491a673422f3b0863ea8288
W. Harrison Wright <wright8191@gmail.com>
2016-11-05 09:06:24 -0500

    Update linux apt-get depends

--- Linux/install.sh
+++ Linux/install.sh
@@ -5,7 +5,9 @@
 DOTFILES_ROOT="$(pwd)"
 
 # install apt-get dependencies
-APT_GET_DEPENDS=("python-dev" "python-pip" "python3-dev" "python3-pip" "vim" "git" "curl" "terminator" "tree")
+APT_GET_DEPENDS=("python-dev" "python-pip" "python3-dev" "python3-pip")
+APT_GET_DEPENDS+=("vim" "git" "curl" "terminator" "tree")
+APT_GET_DEPENDS+=("openssh-client" "openssh-server")
 sudo apt-get update
 for ITEM in "${APT_GET_DEPENDS[@]}"; do
 	echo "Installing ${ITEM}"
