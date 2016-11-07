8466f06afca22a88f5b3fc5b8c0232d909b066b0
Harrison Wright <harrison@blacklocus.com>
2016-11-07 12:27:00 -0600

    Update javarepl install

--- javarepl/install.sh
+++ javarepl/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+. functions.sh
+
 # is this a bad idea?
 DOWNLOAD_LINK="http://albertlatacz.published.s3.amazonaws.com/javarepl/javarepl.jar"
+INSTALL_LOC="/opt/javarepl.jar"
 
-sudo curl --silent --show-error -o "/opt/javarepl.jar" "${DOWNLOAD_LINK}"
+if [ ! -f ${INSTALL_LOC} ]; then
+    sudo curl --silent --show-error -o ${INSTALL_LOC} "${DOWNLOAD_LINK}"
+else
+    info "javarepl already installed"
+fi
