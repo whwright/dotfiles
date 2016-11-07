7d9cf3d73c91fc0e1a39f9420c087ca282252c4c
W. Harrison Wright <wright8191@gmail.com>
2016-11-07 10:29:10 -0600

    Update python install to not be global; and updated the packages installed

--- python/install.sh
+++ python/install.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-# setup global python modules
+# setup global python module
 
-# . functions.sh
+. functions.sh
 
-# MODULES=("virtualenv" "virtualenvwrapper" "virtualfish")
+MODULES=("virtualenv" "virtualfish" "thefuck")
 
-# for MODULE in "${MODULES[@]}"; do
+for MODULE in "${MODULES[@]}"; do
+    pip install --upgrade --user "${MODULE}"
+    if [ $? -ne 0 ]; then
+        fail "error occurred while installed $MODULE"
+    fi
+done
 
-#     sudo -H pip install --upgrade "$MODULE"
+if [ $(uname -s) == "Linux" ]; then
+    mkdir -p "/home/${USER}/.virtualenvs"
+else
+    mkdir -p "/Users/${USER}/.virtualenvs"
+fi
 
-#     if [ $? -ne 0 ]; then
-#         fail "error occurred while installed $MODULE"
-#     fi
-
-#     # TODO: revisit if we want to get rid of the password prompt
-#     #if ! pip freeze | grep $MODULE > /dev/null; then
-#     #    sudo -H pip install --upgrade $MODULE
-#     #fi
-
-# done
-
-# if [ $(uname -s) == "Linux" ]; then
-#     mkdir -p "/home/${USER}/.virtualenvs"
-# else
-#     mkdir -p "/Users/${USER}/.virtualenvs"
-# fi
