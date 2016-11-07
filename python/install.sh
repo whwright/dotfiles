abb468ba6fc1c46f392279fcc867110c1ece51bb
W. Harrison Wright <wright8191@gmail.com>
2016-11-07 08:32:00 -0600

    Comment out python install

--- python/install.sh
+++ python/install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # setup global python modules
 
-. functions.sh
+# . functions.sh
 
-MODULES=("virtualenv" "virtualfish" "thefuck")
+# MODULES=("virtualenv" "virtualenvwrapper" "virtualfish")
 
-for MODULE in "${MODULES[@]}"; do
+# for MODULE in "${MODULES[@]}"; do
 
-    sudo -H pip install --upgrade "$MODULE"
+#     sudo -H pip install --upgrade "$MODULE"
 
-    if [ $? -ne 0 ]; then
-        fail "error occurred while installed $MODULE"
-    fi
+#     if [ $? -ne 0 ]; then
+#         fail "error occurred while installed $MODULE"
+#     fi
 
-    # TODO: revisit if we want to get rid of the password prompt
-    #if ! pip freeze | grep $MODULE > /dev/null; then
-    #    sudo -H pip install --upgrade $MODULE
-    #fi
+#     # TODO: revisit if we want to get rid of the password prompt
+#     #if ! pip freeze | grep $MODULE > /dev/null; then
+#     #    sudo -H pip install --upgrade $MODULE
+#     #fi
 
-done
+# done
 
-if [ ${UNAME} == "Linux" ]; then
-    mkdir -p "/home/${USER}/.virtualenvs"
-else
-    mkdir -p "/Users/${USER}/.virtualenvs"
-fi
+# if [ $(uname -s) == "Linux" ]; then
+#     mkdir -p "/home/${USER}/.virtualenvs"
+# else
+#     mkdir -p "/Users/${USER}/.virtualenvs"
+# fi
