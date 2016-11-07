b8947ae504a0650f9ffb71f32da9e54a45742d23
Harrison Wright <harrison@blacklocus.com>
2016-11-07 12:24:16 -0600

    Remove fisherman from mac config

--- Darwin/fish/config.symlink/fish/config.fish
+++ Darwin/fish/config.symlink/fish/config.fish
@@ -2,11 +2,6 @@
 . ~/.config/fish/bl.fish
 . ~/.config/fish/private.fish
 
-# TODO: do I need this?
-set fisher_home ~/.local/share/fisherman
-set fisher_config ~/.config/fisherman
-source $fisher_home/config.fish
-
 set -x GOPATH ~/go
 set -x PATH $GOPATH/bin $PATH
 
