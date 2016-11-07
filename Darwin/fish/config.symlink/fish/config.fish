c0186332c5e9cd74013a28608556a592a0c9ac1c
Harrison Wright <harrison@blacklocus.com>
2016-11-07 15:08:03 -0600

    Consolidation and organization of fish config

--- Darwin/fish/config.symlink/fish/config.fish
+++ Darwin/fish/config.symlink/fish/config.fish
@@ -1,9 +1,16 @@
-set -x PATH $PATH ~/Library/Python/2.7/bin
-set -x GOPATH ~/go
-set -x PATH $PATH $GOPATH/bin
+#!/usr/bin/env fish
+# fish config for OSX
 
-bass source ~/.nvm/nvm.sh
+# setup local python PATH before calling common config
+set -x PATH ~/Library/Python/2.7/bin $PATH
 
 . ~/.config/fish/common.fish
 . ~/.config/fish/bl.fish
 . ~/.config/fish/private.fish
+
+# nvm
+bass source ~/.nvm/nvm.sh
+
+# golang
+set -x GOPATH ~/go
+set -x $PATH PATH $GOPATH/bin
