bd8d674c8f9d10c44d23451fe5798a5108b86a6b
Harrison Wright <harrison@blacklocus.com>
2016-12-13 14:00:58 -0600

    Move dircolors install to symlink pattern

--- Linux/install.sh
+++ Linux/install.sh
@@ -14,7 +14,3 @@ for ITEM in "${APT_GET_DEPENDS[@]}"; do
 	echo "Installing ${ITEM}"
 	sudo apt-get install -y "${ITEM}"
 done
-
-# dircolors
-mkdir -p "/home/${USER}/.dircolors"
-link_file "${DOTFILES_ROOT}/dircolors/256dark" "/home/${USER}/.dircolors/256dark"
