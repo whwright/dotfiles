1172760071c0d1f47a52555fb046d87d39178f90
W. Harrison Wright <wright8191@gmail.com>
2016-12-15 19:36:01 -0600

    fixed KP_ENTER on Linux

--- zsh/zsh.symlink/Linux.zsh
+++ zsh/zsh.symlink/Linux.zsh
@@ -4,7 +4,16 @@
 PATH=${PATH}:~/.local/bin
 
 # keybind for KP_ENTER (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=685511)
-bindkey "${terminfo[kent]}" accept-line
+if (( ${+terminfo[kent]} )); then
+    kent="${terminfo[kent]}"
+else
+    kent=$(TERM=xterm echoti kent 2>/dev/null)
+    # If there is nothing in `$kent' yet, use "ESC O M", which is
+    # what xterm and urxvt use.
+    [[ -z "$kent" ]] && kent=$'\eOM'
+fi
+bindkey "$kent" accept-line
+unset kent
 
 # eval $(dircolors ${HOME}/.dircolors/256dark)
 
