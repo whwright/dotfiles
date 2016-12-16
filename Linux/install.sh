f06c0ecaf68b52b00d07f5d640ad005270ace9ea
Harrison Wright <harrison@blacklocus.com>
2016-12-15 18:35:24 -0600

    Linux 1 line apt get is better for logs

--- Linux/install.sh
+++ Linux/install.sh
@@ -4,13 +4,7 @@
 . functions.sh
 DOTFILES_ROOT="$(pwd)"
 
-# install apt-get dependencies
-APT_GET_DEPENDS=("vim" "git" "curl" "zsh" "jq")
-APT_GET_DEPENDS+=("tree" "awscli" "tmux" "xclip")
-APT_GET_DEPENDS+=("openssh-client" "openssh-server")
-APT_GET_DEPENDS+=("dmsetup")
 sudo apt-get update
-for ITEM in "${APT_GET_DEPENDS[@]}"; do
-	echo "Installing ${ITEM}"
-	sudo apt-get install -y "${ITEM}"
-done
+sudo apt-get install vim git curl zsh jq tree awscli tmux xclip \
+                     openssh-client openssh-server \
+                     dmsetup
