f69f80564da5c66f29ed50c4b618dc239ca81679
Harrison Wright <harrison@blacklocus.com>
2016-12-15 18:24:17 -0600

    zsh comments and organization

--- zsh/zsh.symlink/Darwin.zsh
+++ zsh/zsh.symlink/Darwin.zsh
@@ -3,9 +3,9 @@ PATH=${PATH}:~/.blscripts
 # Hacky nvm default. https://github.com/creationix/nvm/issues/860
 export PATH=${HOME}/.nvm/versions/node/v0.12.9/bin/:${PATH}
 
+# needs these to go left/right words in iTerm
 bindkey "[D" backward-word
 bindkey "[C" forward-word
-
-# tmux
+# same thing as above but in tmux
 bindkey "OD" backward-word
 bindkey "OC" forward-word
