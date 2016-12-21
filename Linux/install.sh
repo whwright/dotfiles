f0a3f52490b8eb44b32b06469472bccadc61d422
W. Harrison Wright <wright8191@gmail.com>
2016-12-21 15:35:55 -0600

    more linux dependencies

--- Linux/install.sh
+++ Linux/install.sh
@@ -5,6 +5,8 @@
 DOTFILES_ROOT="$(pwd)"
 
 sudo apt-get update
-sudo apt-get install vim git curl zsh jq tree awscli tmux xclip \
-                     openssh-client openssh-server \
-                     dmsetup
+sudo apt-get install -y vim git curl zsh jq tree awscli tmux xclip \
+                        terminator \
+                        acpi \
+                        openssh-client openssh-server \
+                        dmsetup
