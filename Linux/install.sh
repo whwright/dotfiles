ef2168d4a9dd7181b3b118e6a9f4545f8b570b7b
W. Harrison Wright <wright8191@gmail.com>
2016-12-21 15:45:54 -0600

    Reorganize linux depends

--- Linux/install.sh
+++ Linux/install.sh
@@ -4,9 +4,14 @@
 . functions.sh
 DOTFILES_ROOT="$(pwd)"
 
+DEPENDS=("vim" "git" "curl" "zsh" "jq" "tree" "awscli" "tmux" "xclip" "terminator")
+# awesome depends
+DEPENDS+=("acpi" "xbacklight")
+# ssh
+DEPENDS+=("openssh-client" "openssh-server")
+# truecrypt
+DEPENDS+=("dmsetup")
+
+
 sudo apt-get update
-sudo apt-get install -y vim git curl zsh jq tree awscli tmux xclip \
-                        terminator \
-                        acpi \
-                        openssh-client openssh-server \
-                        dmsetup
+sudo apt-get install -y "${DEPENDS[@]}"
