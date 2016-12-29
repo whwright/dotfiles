d70c12e0a05e6c84651be1b51f1256a316586d19
W. Harrison Wright <wright8191@gmail.com>
2016-12-29 16:57:38 -0600

    widgets

--- awesome/config.symlink/awesome/rc.lua
+++ awesome/config.symlink/awesome/rc.lua
@@ -269,14 +269,12 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    if s == 1 then right_layout:add(wibox.widget.systray()) end
-
     right_layout:add(space_separator)
-    right_layout:add(cpu_label)
-    right_layout:add(space_separator)
-    right_layout:add(therm_widget)
-    right_layout:add(space_separator)
-    right_layout:add(cpu_widget)
+    -- right_layout:add(cpu_label)
+    -- right_layout:add(space_separator)
+    -- right_layout:add(therm_widget)
+    -- right_layout:add(space_separator)
+    -- right_layout:add(cpu_widget)
     right_layout:add(separator)
     right_layout:add(mem_widget)
     right_layout:add(separator)
@@ -288,7 +286,9 @@ for s = 1, screen.count() do
     -- icons
     right_layout:add(separator)
     right_layout:add(net_wireless_widget)
+    if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(space_separator)
+    -- awesome layout
     right_layout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
