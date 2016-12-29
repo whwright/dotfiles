8474dfe8e7a3610ec277998fde3b4a6c8de04f3b
W. Harrison Wright <wright8191@gmail.com>
2016-12-29 10:52:44 -0600

    default editor config

--- awesome/config.symlink/awesome/rc.lua
+++ awesome/config.symlink/awesome/rc.lua
@@ -50,7 +50,7 @@ beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
-editor = "vim"
+editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
