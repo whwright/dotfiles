da7ee46081a94d5d6b9a176b2c11c1969e4993c5
W. Harrison Wright <wright8191@gmail.com>
2016-12-30 10:23:37 -0600

    conditional loading for pennypacker

--- awesome/config.symlink/awesome/rc.lua
+++ awesome/config.symlink/awesome/rc.lua
@@ -19,6 +19,18 @@ require("battery_notification")
 -- Load Debian menu entries
 require("debian.menu")
 
+-- get hostname
+function get_hostname()
+  local f = assert(io.popen('hostname', 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', '')
+  return s
+end
+local hostname = get_hostname()
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -188,14 +200,16 @@ vicious.register(cpu_widget, vicious.widgets.cpu, "$1", 3)
 local net_widgets = require("net_widgets")
 net_wireless_widget = net_widgets.wireless({interface="wlp4s0", popup_signal=true})
 -- https://github.com/coldfix/awesome.battery-widget
-local battery_widget = require("battery-widget")
-battery = battery_widget({ adapter         = "BAT0",
-                           battery_prefix  = "Battery: ",
-                           limits          = {
-                            {25, formatting.red},
-                            {50, "orange"},
-                            {100, formatting.green}
-                           }})
+if hostname == "pennypacker" then
+  local battery_widget = require("battery-widget")
+  battery = battery_widget({ adapter         = "BAT0",
+                             battery_prefix  = "Battery: ",
+                             limits          = {
+                              {25, formatting.red},
+                              {50, "orange"},
+                              {100, formatting.green}
+                             }})
+end
 
 -- Create a textclock widget
 mytextclock = awful.widget.textclock("%a %b %d %l:%M%P ", 15)
@@ -282,8 +296,11 @@ for s = 1, screen.count() do
     right_layout:add(cpu_widget)
     right_layout:add(separator)
     right_layout:add(mem_widget)
-    right_layout:add(separator)
-    right_layout:add(battery.widget)
+    -- battery
+    if hostname == "pennypacker" then
+      right_layout:add(separator)
+      right_layout:add(battery.widget)
+    end
     right_layout:add(separator)
     right_layout:add(volume_widget)
     right_layout:add(separator)
