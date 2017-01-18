a89019718239f5433819046cdd7d4397d0f5415a
W. Harrison Wright <wright8191@gmail.com>
2017-01-18 15:58:12 -0600

    Remove autostart in favor of .xsession upstart

--- awesome/config.symlink/awesome/locker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-exec xautolock \
-    -detectsleep \
-    -time 20 \
-    -locker "gnome-screensaver-command --lock" \
-    -notify 60 \
-    -notifier "notify-send -t 5000 --icon=info 'Screen Lock' 'Screen will lock in 1 minute'"
