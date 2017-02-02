e539ab45d3eab442032aca9b6b814f26eb2cfcc7
W. Harrison Wright <wright8191@gmail.com>
2017-02-02 10:48:12 -0600

    Clean up linux install

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # setup linux dependencies
 
-DEPENDS=("vim" "git" "curl" "zsh" "jq" "tree" "awscli" "tmux" "xclip" "terminator")
-DEPENDS+=("acpi" "xbacklight" "xautolock")  # awesome
-DEPENDS+=("openssh-client" "openssh-server")  # ssh
-DEPENDS+=("dmsetup")  # truecrypt
-
 sudo apt-get update
-sudo apt-get install -y "${DEPENDS[@]}"
+sudo apt-get install -y "vim" "git" "curl" "zsh" "jq" "tree" "awscli" "tmux" "xclip" "terminator" \
+                        "acpi" "xbacklight" "xautolock" \
+                        "openssh-client" "openssh-server" \
+                        "dmsetup"
+
