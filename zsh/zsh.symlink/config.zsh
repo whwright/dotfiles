b753de0551ab1ba201303ccc9c084851cc6c39a4
W. Harrison Wright <wright8191@gmail.com>
2017-03-16 11:03:03 -0500

    nvm 6.5.0

--- zsh/zsh.symlink/config.zsh
+++ zsh/zsh.symlink/config.zsh
@@ -46,6 +46,7 @@ fi
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 # Hacky nvm default. https://github.com/creationix/nvm/issues/860
 _safe_load_to_path --load-first "${HOME}/.nvm/versions/node/v4.4.4/bin"
+# _safe_load_to_path --load-first "${HOME}/.nvm/versions/node/v6.5.0/bin"
 # _safe_load_to_path --load-first "${HOME}/.nvm/versions/node/v0.12.9/bin"
 
 # aliases
