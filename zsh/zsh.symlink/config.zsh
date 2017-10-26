6772ae75f6cac068f12a4059bdfa2818816be0f7
W. Harrison Wright <wright8191@gmail.com>
2017-10-26 15:15:32 -0500

    Load PATH stuff before ALIASES

--- zsh/zsh.symlink/config.zsh
+++ zsh/zsh.symlink/config.zsh
@@ -20,46 +20,6 @@ export EDITOR='vim'
 #                                                             -o -fstype 'proc' \\) -prune -o -type d -print 2> /dev/null | cut -b3-"
 # export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 
-#########
-# ALIASES
-#########
-
-# external
-eval "$(thefuck --alias)"
-
-# my aliases
-alias grip="grep -i"
-alias psg="ps -ef | grep"
-alias pubip="curl http://canihazip.com/s/; echo ''"
-alias cll="clear; ll"
-alias javarepl="java -jar /opt/javarepl.jar"
-alias reload="source ~/.zshrc"
-alias pyjson="python -m json.tool"
-alias mylsblk="lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL"
-alias xo="xdg-open"
-# OSX compatible copy/paste
-alias pbcopy="xclip -selection c"
-alias pbpaste="xclip -selection clipboard -o"
-
-##########
-# KEYBINDS
-##########
-
-# keybind for KP_ENTER (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=685511)
-if (( ${+terminfo[kent]} )); then
-    kent="${terminfo[kent]}"
-else
-    kent=$(TERM=xterm echoti kent 2>/dev/null)
-    # If there is nothing in `$kent' yet, use "ESC O M", which is
-    # what xterm and urxvt use.
-    [[ -z "$kent" ]] && kent=$'\eOM'
-fi
-bindkey "$kent" accept-line
-unset kent
-
-bindkey "^[[1;3D" backward-word
-bindkey "^[[1;3C" forward-word
-
 ######
 # PATH
 ######
@@ -99,6 +59,46 @@ _safe_load_to_path "${HOME}/.blscripts"
 _safe_load_to_path "/usr/local/go/bin"   # go
 _safe_load_to_path "${HOME}/.local/bin"  # python
 _safe_load_to_path "${HOME}/.cargo/bin"  # rust
+
+#########
+# ALIASES
+#########
+
+# external
+eval "$(thefuck --alias)"
+
+# my aliases
+alias grip="grep -i"
+alias psg="ps -ef | grep"
+alias pubip="curl http://canihazip.com/s/; echo ''"
+alias cll="clear; ll"
+alias javarepl="java -jar /opt/javarepl.jar"
+alias reload="source ~/.zshrc"
+alias pyjson="python -m json.tool"
+alias mylsblk="lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL"
+alias xo="xdg-open"
+# OSX compatible copy/paste
+alias pbcopy="xclip -selection c"
+alias pbpaste="xclip -selection clipboard -o"
+
+##########
+# KEYBINDS
+##########
+
+# keybind for KP_ENTER (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=685511)
+if (( ${+terminfo[kent]} )); then
+    kent="${terminfo[kent]}"
+else
+    kent=$(TERM=xterm echoti kent 2>/dev/null)
+    # If there is nothing in `$kent' yet, use "ESC O M", which is
+    # what xterm and urxvt use.
+    [[ -z "$kent" ]] && kent=$'\eOM'
+fi
+bindkey "$kent" accept-line
+unset kent
+
+bindkey "^[[1;3D" backward-word
+bindkey "^[[1;3C" forward-word
 
 #############
 # PROGRAMMING
