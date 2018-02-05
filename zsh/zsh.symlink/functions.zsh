3e07117f6f786040fe7f26f9837b91a58e66ffa2
W. Harrison Wright <wright8191@gmail.com>
2018-02-05 16:22:48 -0600

    Clean up some zsh function

--- zsh/zsh.symlink/functions.zsh
+++ zsh/zsh.symlink/functions.zsh
@@ -1,55 +1,11 @@
 #!/usr/bin/env zsh
 # zsh functions; alphabetized
 
-# awesome notification
-# https://www.reddit.com/r/awesomewm/comments/1gyt8x/notifications_for_completed_commands/
-function aalert {
-    RVAL=$?                         # get return value of the last command
-    DATE=$(date)                    # get time of completion
-    LAST=${history[${HISTCMD}]}     # get current command
-    LAST=${LAST%[;&|]*}             # remove "; alert" from it
-
-    # set window title so we can get back to it
-    echo -ne "\e]2;$LAST\a"
-
-    LAST=${LAST//\"/'\"'}        # replace " for \" to not break lua format
-
-    # check if the command was successful
-    if [[ ${RVAL} == 0 ]]; then
-        RVAL="SUCCESS"
-        BG_COLOR="beautiful.bg_normal"
-        FG_COLOR="beautiful.fg_normal"
-    else
-        RVAL="FAILURE"
-        BG_COLOR="beautiful.bg_urgent"
-        FG_COLOR="beautiful.fg_urgent"
-    fi
-
-    # compose the notification
-    MESSAGE="naughty.notify({ \
-                title   = \"Command completed on: \t\t${DATE}\", \
-                text    = \"↪ ${LAST}\" .. newline .. \"${RVAL}\", \
-                timeout = 0, \
-                bg      = ${BG_COLOR},
-                fg      = ${FG_COLOR},
-                width   = 430,
-            })"
-    # send it to awesome
-    echo ${MESSAGE} | awesome-client
-}
-
 # cd into last item in current directory
 function cdlast() {
     last=$(ls -d */ | tail -1)
     cd ${last}
 }
-
-# fh - repeat history
-# https://github.com/junegunn/fzf/wiki/examples#command-history
-function fh {
-  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
-}
-
 
 # finds dirty git repos
 # uses $PWD or the 1st argument given as the root to look through
@@ -125,7 +81,7 @@ function tmux_killssh() {
 }
 alias tkssh="tmux_killssh"
 
-function blog {
+function blog() {
     local bl_log_base="${HOME}/s3/bl-log"
     if [ $# -ne 1 ]; then
         echo "usage: blog [ASG]"
