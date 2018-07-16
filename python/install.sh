b8f9eed13d873abd0b95b4c8c081bcbd60026160
W. Harrison Wright <wright8191@gmail.com>
2018-07-16 12:19:41 -0500

    Start of debinate packages

---
+++ python/install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# setup python things
+
+set -o errexit
+set -o nounset
+set -o pipefail
+source functions.sh
+
+# TODO: revisit this (https://github.com/whwright/dotfiles/issues/28)
+# sudo apt-get -qq update
+# sudo apt-get -qq install python-dev python-pip python-setuptools \
+#                         python3-dev python3-pip python3-setuptools
+#
+# global modules
+# sudo -H pip install --quiet --upgrade pip virtualenv virtualenvwrapper
+#
+# make sure virtualenvwrapper location exists
+# mkdir -p "/home/${USER}/.virtualenvs"
+
+# all submodules in python/ should be debinate projects
+# and python3
+info "Installing debinate packages..."
+export DEBINATE_PYTHON=$(which python3)
+for module in $(git submodule--helper list | grep "python/" | awk '{print $4}'); do
+    projet_name=${module##*/}
+    if ! [ -d "${module}/.debinate" ]; then
+        echo "${module}" is not a debinate projects
+        exit 1
+    fi
+
+    info "Building ${module}"
+    pushd ${module} > /dev/null
+    debinate clean > /dev/null
+    debinate package > /dev/null
+    popd > /dev/null
+
+    debs=($(find ${module} -name *.deb))
+
+    user "install ${debs[0]}?"
+    read -p "" action
+    case "${action}" in
+        y)
+            info "Installing ${debs[0]}"
+            sudo dpkg -i ${debs[0]}
+            sudo ln -sv /opt/${projet_name}/.virtualenv/bin/${projet_name} /usr/local/bin/${projet_name}
+            ;;
+        n)
+            echo "Not installing"
+            ;;
+        *)
+            echo "Unknown choice"
+            ;;
+    esac
+done
