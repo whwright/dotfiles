294391d47ff8e836289eac4227cb4a13c7ada1c3
Harrison Wright <mail@harrisonwright.me>
2019-04-15 00:17:54 -0500

    Add system zsh configs

---
+++ zsh/zsh.symlink/Darwin.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+########
+# PATH #
+########
+
+# TODO: idk if I still need this
+# https://github.com/whwright/dotfiles/commit/faca65a233b3c52cdbe851f9240114a992fffdba#diff-5ec2d84965e1bb264bcfe18b5f7e54c0
+
+# _safe_load_to_path "${HOME}/Library/Python/2.7/bin"
+
+############
+# KEYBINDS #
+############
+
+# # needs these to go left/right words in iTerm
+# bindkey "[D" backward-word
+# bindkey "[C" forward-word
+# # same thing as above but in tmux
+# bindkey "OD" backward-word
+# bindkey "OC" forward-word
