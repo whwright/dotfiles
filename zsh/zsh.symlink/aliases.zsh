448651c41ff62762953e9f0b299f162b2f1a5809
Harrison Wright <harrison.wright@opcity.com>
2019-04-23 13:55:16 -0500

    More OSX compat

--- zsh/zsh.symlink/aliases.zsh
+++ zsh/zsh.symlink/aliases.zsh
@@ -7,7 +7,7 @@ source $(which virtualenvwrapper_lazy.sh)
 # my aliases
 alias grip="grep -i"
 alias ll="ls -lhN"
+alias la="ls -lAhN"
 alias psg="ps -ef | grep"
 alias pyjson="python -m json.tool"
 alias reload="source ~/.zshrc"
-
