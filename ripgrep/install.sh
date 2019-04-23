d72e992b68ab8eda7dc27f23817dedfe76c42517
Harrison Wright <harrison.wright@opcity.com>
2019-04-23 12:59:11 -0500

    ripgrep dual support

--- ripgrep/install.sh
+++ ripgrep/install.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
 source lib.sh
 
-DEB_FILE_URL="https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb"
-DEST="/tmp/$(basename ${DEB_FILE_URL})"
+install_ripgrep() {
+    info "Installing ripgrep..."
+    case "$(uname -s)" in
+        Linux)
+            DEB_FILE_URL="https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb"
+            DEST="/tmp/$(basename ${DEB_FILE_URL})"
+            curl --silent --show-error -o "${DEST}" -L "${DEB_FILE_URL}"
+            sudo dpkg -i "${DEST}"
+            ;;
+        Darwin)
+            brew install ripgrep
+            ;;
+        *)
+            fail "Unsupported OS: $(uname -s)"
+            exit 1
+            ;;
+    esac
+}
 
-dpkg -l ripgrep > /dev/null
-if [ $? -eq 0 ]; then
+# install zsh
+if ! type rg > /dev/null 2>&1; then
+    install_ripgrep
+else
     info "ripgrep already installed"
-    exit 0
 fi
-
-curl --silent --show-error -o "${DEST}" -L "${DEB_FILE_URL}"
-sudo dpkg -i "${DEST}"
-
