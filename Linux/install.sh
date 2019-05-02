d2ba8547741536aa21a6ff39c437429f033abafd
Rees Dooley <rees@blacklocus.com>
2019-05-02 10:17:03 -0400

    Forgot light-locker

--- Linux/install.sh
+++ Linux/install.sh
@@ -5,9 +5,11 @@ set -o nounset
 set -o pipefail
 source lib.sh
 
+sudo add-apt-repository ppa:light-locker/release
+
 sudo apt-get -qq update
 sudo apt-get -qq install -y "vim" "git" "curl" "jq" "tree" "awscli" "tmux" "xclip" "terminator" \
-                        "acpi" "ack-grep" "xbacklight" "xautolock" \
+                        "acpi" "ack-grep" "xbacklight" "xautolock" "light-locker" \
                         "openssh-client" "openssh-server" \
                         "dmsetup"
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
