8855ed37180a7658c36c16df7c62d74b90ca9fd0
Rees Dooley <rees@blacklocus.com>
2019-05-02 09:49:55 -0400

    More pruning and editing

--- Linux/install.sh
+++ Linux/install.sh
@@ -7,6 +7,7 @@ source lib.sh
 
 sudo apt-get -qq update
 sudo apt-get -qq install -y "vim" "git" "curl" "jq" "tree" "awscli" "tmux" "xclip" "terminator" \
-                        "acpi" "xbacklight" "xautolock" \
+                        "acpi" "ack-grep" "xbacklight" "xautolock" \
                         "openssh-client" "openssh-server" \
                         "dmsetup"
+sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
