e3e45cf642f2c4d7284bdd9e3e0ed87804322e79
Harrison Wright <harrison.wright@opcity.com>
2019-07-22 09:37:59 -0500

    Remove old mac keybinds

--- zsh/zsh.symlink/keybinds.zsh
+++ zsh/zsh.symlink/keybinds.zsh
@@ -1,13 +1,6 @@
 #!/usr/bin/env zsh
 
-if [ "$(uname -s)" = "Darwin" ]; then
-    # needs these to go left/right words in iTerm
-    bindkey "[D" backward-word
-    bindkey "[C" forward-word
-    # same thing as above but in tmux
-    bindkey "OD" backward-word
-    bindkey "OC" forward-word
-elif [ "$(uname -s)" = "Linux" ]; then
+if [ "$(uname -s)" = "Linux" ]; then
     # keybind for KP_ENTER (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=685511)
     if (( ${+terminfo[kent]} )); then
         kent="${terminfo[kent]}"
