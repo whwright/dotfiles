f4b836a033836a09c0693a03b809d841f4d32db5
Harrison Wright <mail@harrisonwright.me>
2020-08-24 16:45:31 -0500

    Fix python isntall

--- python/install.sh
+++ python/install.sh
@@ -5,15 +5,14 @@ set -o nounset
 set -o pipefail
 source lib.sh
 
-# we need pip first
+if [[ "$(uname -s)" = "Linux" ]]; then
+    sudo apt-get update
+    sudo apt install python3-dev python3-distutils python3-venv
+fi
+
 if ! type pip > /dev/null 2>&1; then
     curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
     sudo python3 /tmp/get-pip.py
-fi
-
-if [[ "$(uname -s)" = "Linux" ]]; then
-    sudo apt-get update
-    sudo apt install python3-venv
 fi
 
 # global packages
