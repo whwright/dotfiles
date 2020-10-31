0f40fe485208174eb48ed8f6354bb8176ef9352e
Harrison Wright <mail@harrisonwright.me>
2020-10-31 10:45:35 -0500

    Update python install

--- python/install.sh
+++ python/install.sh
@@ -3,16 +3,19 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
 
 if [[ "$(uname -s)" = "Linux" ]]; then
     sudo apt-get -qq update
-    sudo apt -qq install python-minimal python3-dev python3-distutils python3-venv
+    sudo apt -qq install python-minimal python-pip python3-pip python3-dev python3-distutils python3-venv
 fi
 
-if ! type pip > /dev/null 2>&1; then
-    curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-    sudo python3 /tmp/get-pip.py
-fi
+# TODO: this was conflicting with system pip and creating a different workflow than
+# TODO: I am used to; look into this again
+#if ! type pip > /dev/null 2>&1; then
+#    curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+#    sudo python3 /tmp/get-pip.py
+#fi
 
 # global packages
 sudo -H pip install --quiet --upgrade pip virtualenv
