82519a85088f1d0eca1b0b51e5cbde05bbe4e23a
Harrison Wright <harrison@setpoint.io>
2024-01-18 21:42:44 -0600

    rejigger sublime install

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-# setup Sublime Text 3
+# setup Sublime Text
 
 set -o errexit
 set -o nounset
 set -o pipefail
 source lib.sh
 
-# TODO: it's actually sublime 4 now
-# install sublime3 config
-# TODO: on first install SUBLIME_DIR will not be defined because it's in zsh config
-# TODO: need to install sublime and install package control first
+if [ "$(uname -s)" = "Darwin" ]; then
+    SUBLIME_DIR="${HOME}/Library/Application Support/Sublime Text"
+elif [ "$(uname -s)" = "Linux" ]; then
+    SUBLIME_DIR="${HOME}/.config/sublime-text-3"
+fi
+
+if [[ ! -d "${SUBLIME_DIR}" ]]; then
+    fail "Install Sublime Text and Package Control before running this script"
+    exit 1
+fi
+
 if [ ! -L "${SUBLIME_DIR}/Packages/User" ]; then
-    info "Installing sublime3 config..."
+    info "Installing sublime config..."
     mv "${SUBLIME_DIR}/Packages/User" "${SUBLIME_DIR}/Packages/User.backup"
-    ln -s ~/.dotfiles/sublime3/User "${SUBLIME_DIR}/Packages/User"
+    ln -s ~/.dotfiles/sublime/User "${SUBLIME_DIR}/Packages/User"
     success "sublime3 config installed"
 else
     info "sublime3 config already installed"
 fi
 
 if [ "$(uname -s)" == "Darwin" ] && [ ! -f /usr/local/bin/subl ]; then
-    # TODO: use link_file
     info "Linking subl binary"
     sudo ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 fi
