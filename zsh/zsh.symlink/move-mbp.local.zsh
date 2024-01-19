c9d11ae8c4da507f11ec2651b71bfe500403a791
Harrison Wright <harrison@setpoint.io>
2024-01-18 21:20:06 -0600

    chore: remove old hostname configs

--- zsh/zsh.symlink/move-mbp.local.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-export ANDROID_HOME=${HOME}/Library/Android/sdk
-export ANDROID_SDK_ROOT=${HOME}/Library/Android/sdk
-
-# virtualenvwrapper
-export VIRTUALENVWRAPPER_PYTHON=/opt/homebrew/bin/python3
-source $(which virtualenvwrapper_lazy.sh)
