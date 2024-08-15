41bc7b0305fa25dad11cc628157e940fc0439f25
Harrison Wright <harrison@setpoint.io>
2024-08-15 14:43:32 -0500

    wezterm key binds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Import the wezterm module
 local wezterm = require 'wezterm'
+local default_keys = require 'default_keys'
 
 wezterm.log_info("hello world! my name is " .. wezterm.hostname())
 
@@ -24,38 +25,14 @@ config.font = wezterm.font({
 config.window_decorations = 'RESIZE'
 
 
--- config.send_composed_key_when_left_alt_is_pressed = true
--- config.use_ime = false
--- config.allow_win32_input_mode = false
-config.disable_default_key_bindings = true
-
-config.keys = {
+keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
     {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
     -- Make Option-Right equivalent to Alt-f; forward-word
     {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 }
+for k, v in pairs(default_keys) do keys[k] = v end
+config.keys = keys
 
 
--- Returns our config to be evaluated. We must always do this at the bottom of this file
 return config
-
-
-
--- -- config.allow_win32_input_mode = false
--- -- config.send_composed_key_when_left_alt_is_pressed = false
--- -- config.use_ime = false
-
--- config.keys = {
---     -- { key="LeftOption", action=act.SendString("+Esc") }
--- }
-
--- -- If you have your own leader key, make sure to set it before loading this
--- -- plugin.
--- config.leader = { key = "\\", mods = "CTRL" }
-
--- -- Add these lines:
--- require("wez-tmux.plugin").apply_to_config(config, {})
-
--- -- and finally, return the configuration to wezterm
--- return config
