9e9b6b11bfd9535e76ce21d1ef021f61f4593110
Harrison Wright <harrison@setpoint.io>
2024-08-31 12:29:06 -0500

    wezterm updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 -- Import the wezterm module
-local wezterm = require 'wezterm'
-local default_keys = require 'default_keys'
+local wezterm = require("wezterm")
+local default_keys = require("default_keys")
 
 wezterm.log_info("hello world! my name is " .. wezterm.hostname())
 
@@ -18,28 +18,29 @@ config.color_scheme = "Idle Toes (Gogh)"
 
 -- disable ligatures
 config.font = wezterm.font({
-    family = "JetBrains Mono",
-    weight = "Medium",
-    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	family = "JetBrains Mono",
+	weight = "Medium",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
+config.font_size = 13
 
 -- Removes the title bar, leaving only the tab bar
 if is_darwin then
-    config.window_decorations = 'RESIZE'
+	config.window_decorations = "RESIZE"
 end
 config.hide_tab_bar_if_only_one_tab = true
-
 
 -- Disable default keys, and then bring them back in via default_keys.lua
 config.disable_default_key_bindings = true
 keys = {
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
-for k, v in pairs(default_keys) do keys[k] = v end
+for k, v in pairs(default_keys) do
+	keys[k] = v
+end
 config.keys = keys
-
 
 return config
