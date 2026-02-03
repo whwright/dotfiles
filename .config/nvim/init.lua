0620762ef9c0acbd49d3d946f1b23730f6bc3246
Harrison Wright <harrison@setpoint.io>
2026-02-02 19:22:29 -0600

    black formatter in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -768,11 +768,18 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'black' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        black = {
+          command = 'uvx',
+          prepend_args = { 'black' },
+        },
       },
     },
   },
