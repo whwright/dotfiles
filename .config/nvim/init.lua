78a6015def549950726cc7de675f1a1d4722796c
Harrison Wright <harrison@setpoint.io>
2026-07-07 16:41:16 -0500

    idk python lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -715,8 +715,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        ty = {},
+        pyright = {},
+        -- ty = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
